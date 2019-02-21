c5d324af728840586fd0abf8695a67c5c3e5fa31
splexpe <46957762+splexpe@users.noreply.github.com>
2019-02-20 23:28:01 -0600

    Update Main.php

--- src/pvpshop/Main.php
+++ src/pvpshop/Main.php
@@ -19,7 +19,8 @@ use pocketmine\command\{Command, CommandSender, ConsoleCommandSender, CommandExe
 
 use onebone\economyapi\Economy;
 
-class Main extends PluginBase implements Listener {
+class Main extends PluginBase implements Listener
+{
     public $shop;
     public $item;
 
@@ -28,26 +29,27 @@ class Main extends PluginBase implements Listener {
     "Item name" => [item_id, item_damage, buy_price, sell_price]
     */
     public $Armor = [
-        "ICON" => ["Armor",311,0],
-        "Diamond Helmet" => [310,0,1000,0],
-        "Diamond Chestplate" => [311,0,2500,0],
-        "Diamond Leggings" => [312,0,1500,0],
-        "Diamond Boots" => [313,0,1000,0]
+        "ICON" => ["Armor", 311, 0],
+        "Diamond Helmet" => [310, 0, 1000, 0],
+        "Diamond Chestplate" => [311, 0, 2500, 0],
+        "Diamond Leggings" => [312, 0, 1500, 0],
+        "Diamond Boots" => [313, 0, 1000, 0]
     ];
 
     public $Potions = [
-        "ICON" => ["Potions",373,0],
-        "Strength" => [373,33,1000,100],
-        "Regeneration" => [373,28,1000,100],
-        "Speed" => [373,16,1000,500],
-        "Fire Resistance" => [373,13,1000,100],
-        "Poison (SPLASH)" => [438,22,1000,100],
-        "Poison (SPLASH)" => [438,27,1000,100],
-        "Weakness (SPLASH)" => [438,35,1000,100],
-        "Slowness (SPLASH)" => [438,17,1000,100]
+        "ICON" => ["Potions", 373, 0],
+        "Strength" => [373, 33, 1000, 100],
+        "Regeneration" => [373, 28, 1000, 100],
+        "Speed" => [373, 16, 1000, 500],
+        "Fire Resistance" => [373, 13, 1000, 100],
+        "Poison (SPLASH)" => [438, 22, 1000, 100],
+        "Poison (SPLASH)" => [438, 27, 1000, 100],
+        "Weakness (SPLASH)" => [438, 35, 1000, 100],
+        "Slowness (SPLASH)" => [438, 17, 1000, 100]
     ];
 
-    public function onEnable(){
+    public function onEnable()
+    {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         PacketPool::registerPacket(new GuiDataPickItemPacket());
         PacketPool::registerPacket(new ModalFormRequestPacket());
@@ -57,13 +59,14 @@ class Main extends PluginBase implements Listener {
         $this->item = [$this->Potions, $this->Armor];
     }
 
-    public function sendMainShop(Player $player){
+    public function sendMainShop(Player $player)
+    {
         $ui = new SimpleForm("§cPvp Shop");
-        foreach($this->item as $category){
-            if(isset($category["ICON"])){
+        foreach ($this->item as $category) {
+            if (isset($category["ICON"])) {
                 $rawitemdata = $category["ICON"];
                 $button = new Button($rawitemdata[0]);
-                $button->addImage('url', "http://avengetech.me/items/".$rawitemdata[1]."-".$rawitemdata[2].".png");
+                $button->addImage('url', "http://avengetech.me/items/" . $rawitemdata[1] . "-" . $rawitemdata[2] . ".png");
                 $ui->addButton($button);
             }
         }
@@ -74,19 +77,20 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function sendShop(Player $player, $id){
+    public function sendShop(Player $player, $id)
+    {
         $ui = new SimpleForm("§cPvP Shop");
         $ids = -1;
-        foreach($this->item as $category){
+        foreach ($this->item as $category) {
             $ids++;
             $rawitemdata = $category["ICON"];
-            if($ids == $id){
+            if ($ids == $id) {
                 $name = $rawitemdata[0];
                 $data = $this->$name;
-                foreach($data as $name => $item){
-                    if($name != "ICON"){
+                foreach ($data as $name => $item) {
+                    if ($name != "ICON") {
                         $button = new Button($name);
-                        $button->addImage('url', "http://avengetech.me/items/".$item[0]."-".$item[1].".png");
+                        $button->addImage('url', "http://avengetech.me/items/" . $item[0] . "-" . $item[1] . ".png");
                         $ui->addButton($button);
                     }
                 }
@@ -99,18 +103,19 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function sendConfirm(Player $player, $id){
+    public function sendConfirm(Player $player, $id)
+    {
         $ids = -1;
         $idi = -1;
-        foreach($this->item as $category){
+        foreach ($this->item as $category) {
             $ids++;
             $rawitemdata = $category["ICON"];
-            if($ids == $this->shop[$player->getName()]){
+            if ($ids == $this->shop[$player->getName()]) {
                 $name = $rawitemdata[0];
                 $data = $this->$name;
-                foreach($data as $name => $item){
-                    if($name != "ICON"){
-                        if($idi == $id){
+                foreach ($data as $name => $item) {
+                    if ($name != "ICON") {
+                        if ($idi == $id) {
                             $this->item[$player->getName()] = $id;
                             $iname = $name;
                             $cost = $item[2];
@@ -124,10 +129,10 @@ class Main extends PluginBase implements Listener {
         }
 
         $ui = new CustomForm($iname);
-        $slider = new Slider("§dAmount ",1,500,0);
+        $slider = new Slider("§dAmount ", 1, 500, 0);
         $toggle = new Toggle("§5Selling");
-        if($sell == 0) $sell = "0";
-        $label = new Label(TF::GREEN."Buy: $".TF::GREEN.$cost.TF::RED."\nSell: $".TF::RED.$sell);
+        if ($sell == 0) $sell = "0";
+        $label = new Label(TF::GREEN . "Buy: $" . TF::GREEN . $cost . TF::RED . "\nSell: $" . TF::RED . $sell);
         $ui->addElement($label);
         $ui->addElement($toggle);
         $ui->addElement($slider);
@@ -138,32 +143,33 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function sell(Player $player, $data, $amount){
+    public function sell(Player $player, $data, $amount)
+    {
         $ids = -1;
         $idi = -1;
-        foreach($this->item as $category){
+        foreach ($this->item as $category) {
             $ids++;
             $rawitemdata = $category["ICON"];
-            if($ids == $this->shop[$player->getName()]){
+            if ($ids == $this->shop[$player->getName()]) {
                 $name = $rawitemdata[0];
                 $data = $this->$name;
-                foreach($data as $name => $item){
-                    if($name != "ICON"){
-                        if($idi == $this->item[$player->getName()]){
+                foreach ($data as $name => $item) {
+                    if ($name != "ICON") {
+                        if ($idi == $this->item[$player->getName()]) {
                             $iname = $name;
                             $id = $item[0];
                             $damage = $item[1];
-                            $cost = $item[2]*$amount;
-                            $sell = $item[3]*$amount;
-                            if($sell == 0){
+                            $cost = $item[2] * $amount;
+                            $sell = $item[3] * $amount;
+                            if ($sell == 0) {
                                 $player->sendMessage(TF::BOLD . TF::DARK_GRAY . "(" . TF::RED . "!" . TF::DARK_GRAY . ") " . TF::RESET . TF::GRAY . "§cThis is not sellable!");
                                 return true;
                             }
-                            if($player->getInventory()->contains(Item::get($id,$damage,$amount))){
-                                $player->getInventory()->removeItem(Item::get($id,$damage,$amount));
+                            if ($player->getInventory()->contains(Item::get($id, $damage, $amount))) {
+                                $player->getInventory()->removeItem(Item::get($id, $damage, $amount));
                                 EconomyAPI::getInstance()->addMoney($player, $sell);
                                 $player->sendMessage(TF::BOLD . TF::DARK_GRAY . "(" . TF::GREEN . "!" . TF::DARK_GRAY . ") " . TF::RESET . TF::GRAY . "§bYou have sold §3$amount $iname §bfor §3$$sell");
-                            }else{
+                            } else {
                                 $player->sendMessage(TF::BOLD . TF::DARK_GRAY . "(" . TF::RED . "!" . TF::DARK_GRAY . ") " . TF::RESET . TF::GRAY . "§2You do not have §5$amount $iname!");
                             }
                             unset($this->item[$player->getName()]);
@@ -178,28 +184,29 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function purchase(Player $player, $data, $amount){
+    public function purchase(Player $player, $data, $amount)
+    {
         $ids = -1;
         $idi = -1;
-        foreach($this->item as $category){
+        foreach ($this->item as $category) {
             $ids++;
             $rawitemdata = $category["ICON"];
-            if($ids == $this->shop[$player->getName()]){
+            if ($ids == $this->shop[$player->getName()]) {
                 $name = $rawitemdata[0];
                 $data = $this->$name;
-                foreach($data as $name => $item){
-                    if($name != "ICON"){
-                        if($idi == $this->item[$player->getName()]){
+                foreach ($data as $name => $item) {
+                    if ($name != "ICON") {
+                        if ($idi == $this->item[$player->getName()]) {
                             $iname = $name;
                             $id = $item[0];
                             $damage = $item[1];
-                            $cost = $item[2]*$amount;
-                            $sell = $item[3]*$amount;
-                            if(EconomyAPI::getInstance()->myMoney($player) > $cost){
-                                $player->getInventory()->addItem(Item::get($id,$damage,$amount));
+                            $cost = $item[2] * $amount;
+                            $sell = $item[3] * $amount;
+                            if (EconomyAPI::getInstance()->myMoney($player) > $cost) {
+                                $player->getInventory()->addItem(Item::get($id, $damage, $amount));
                                 EconomyAPI::getInstance()->reduceMoney($player, $cost);
                                 $player->sendMessage(TF::BOLD . TF::DARK_GRAY . "(" . TF::GREEN . "!" . TF::DARK_GRAY . ") " . TF::RESET . TF::GRAY . "§bYou purchased §3$amount $iname §bfor §3$$cost");
-                            }else{
+                            } else {
                                 $player->sendMessage(TF::BOLD . TF::DARK_GRAY . "(" . TF::RED . "!" . TF::DARK_GRAY . ") " . TF::RESET . TF::GRAY . "§2You do not have enough money to buy §5$amount $iname");
                             }
                             unset($this->item[$player->getName()]);
@@ -214,28 +221,29 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function DataPacketReceiveEvent(DataPacketReceiveEvent $event){
+    public function DataPacketReceiveEvent(DataPacketReceiveEvent $event)
+    {
         $packet = $event->getPacket();
         $player = $event->getPlayer();
-        if($packet instanceof ModalFormResponsePacket){
+        if ($packet instanceof ModalFormResponsePacket) {
             $id = $packet->formId;
             $data = $packet->formData;
             $data = json_decode($data);
-            if($data === Null) return true;
-            if($id === 110){
+            if ($data === Null) return true;
+            if ($id === 110) {
                 $this->shop[$player->getName()] = $data;
                 $this->sendShop($player, $data);
                 return true;
             }
-            if($id === 111){
+            if ($id === 111) {
                 //$this->shop[$player->getName()] = $data;
                 $this->sendConfirm($player, $data);
                 return true;
             }
-            if($id === 112){
+            if ($id === 112) {
                 $selling = $data[1];
                 $amount = $data[2];
-                if($selling){
+                if ($selling) {
                     $this->sell($player, $data, $amount);
                     return true;
                 }
@@ -246,12 +254,12 @@ class Main extends PluginBase implements Listener {
         return true;
     }
 
-    public function onCommand(CommandSender $player, Command $command, string $label, array $args) : bool{
-        switch(strtolower($command)){
+    public function onCommand(CommandSender $player, Command $command, string $label, array $args): bool
+    {
+        switch (strtolower($command)) {
             case "pshop":
                 $this->sendMainShop($player);
                 return true;
+            }
         }
     }
-    return true;
-}
